4b3817d898198f44732adfd53bc65e5dbed463ee
Charlespartina <tangziyi001@gmail.com>
2018-12-01 16:07:05 -0500

    basic action

---
+++ NewDawn/NewDawn/RegisterUserViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RegisterUserViewController.swift
+//  NewDawn
+//
+//  Created by 汤子毅 on 2018/12/1.
+//  Copyright © 2018 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        print("Signup")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NewDawn/NewDawn/SignInViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignInViewController.swift
+//  NewDawn
+//
+//  Created by 汤子毅 on 2018/12/1.
+//  Copyright © 2018 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        print("Sign in button tapped")
+    }
+
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        let registerUserViewController =
+            self.storyboard?.instantiateViewController(
+                withIdentifier: "RegisterUserViewController") as! RegisterUserViewController
+        self.present(registerUserViewController, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
