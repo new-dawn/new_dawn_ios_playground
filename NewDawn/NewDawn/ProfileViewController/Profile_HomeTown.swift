4caed5b14889d58806bd506cf15a67a1ab05399a
tianchuxie <TIANCHU.XIE@GMAIL.COM>
2018-12-23 12:48:45 -0500

    add the blank story boards and files linked to it, each memeber of the team can take responosable for single storyboard and viewController

---
+++ NewDawn/NewDawn/ProfileViewController/Profile_HomeTown.swift
@@ -0,0 +1,30 @@
+//
+//  Profile_HomeTown.swift
+//  NewDawn
+//
+//  Created by macboy on 12/23/18.
+//  Copyright © 2018 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class Profile_HomeTown: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
