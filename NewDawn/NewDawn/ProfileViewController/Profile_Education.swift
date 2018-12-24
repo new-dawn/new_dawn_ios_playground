e2bbbcfa1b0ef33033c348251a0bdee77861f8ab
Charlespartina <tangziyi001@gmail.com>
2018-12-24 14:07:04 -0800

    Picker

--- NewDawn/NewDawn/ProfileViewController/Profile_Education.swift
+++ NewDawn/NewDawn/ProfileViewController/Profile_Education.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class Profile_Education: UIViewController {
+class Profile_Education: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var visible = false
+    let degreePickerData = [String](arrayLiteral: "High School", "Undergrad", "Grad", "PhD")
+    
     @IBOutlet weak var degreeTextField: UITextField!
     @IBOutlet weak var schoolTextField: UITextField!
     @IBOutlet weak var visibleButton: UIButton!
@@ -20,6 +22,19 @@ class Profile_Education: UIViewController {
         polishTextField(textField: schoolTextField)
         polishTextField(textField: degreeTextField)
         polishUIButton(button: visibleButton)
+        
+        // Picker Toolbar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: #selector(Profile_Education.donePicker))
+        toolbar.setItems([doneButton], animated: false)
+        toolbar.isUserInteractionEnabled = true
+        
+        // Degree Picker
+        let degreePicker = UIPickerView()
+        degreeTextField.inputView = degreePicker
+        degreeTextField.inputAccessoryView = toolbar
+        degreePicker.delegate = self
     }
     
     @IBAction func visibleButtonTapped(_ sender: Any) {
@@ -30,6 +45,26 @@ class Profile_Education: UIViewController {
             selectButton(button: visibleButton, text: "Visible")
             visible = true
         }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return degreePickerData.count
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return degreePickerData[row]
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        degreeTextField.text = degreePickerData[row]
+    }
+    
+    @objc func donePicker() {
+        degreeTextField.resignFirstResponder()
     }
     
     /*
