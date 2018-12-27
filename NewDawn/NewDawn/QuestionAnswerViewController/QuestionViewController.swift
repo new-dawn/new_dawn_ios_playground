ccff19383f35af2558dd36eced547c0a45bd657f
Charlespartina <tangziyi001@gmail.com>
2018-12-27 10:23:21 -0800

    qa

---
+++ NewDawn/NewDawn/QuestionAnswerViewController/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  NewDawn
+//
+//  Created by 汤子毅 on 2018/12/27.
+//  Copyright © 2018 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
