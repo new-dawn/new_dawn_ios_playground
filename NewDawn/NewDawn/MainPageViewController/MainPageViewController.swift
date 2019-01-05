c1d708dcc8ef955af830218ffcb7ec92368e3a7d
Charlespartina <tangziyi001@gmail.com>
2019-01-04 21:17:23 -0500

    Main Page

---
+++ NewDawn/NewDawn/MainPageViewController/MainPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainPageViewController.swift
+//  NewDawn
+//
+//  Created by 汤子毅 on 2019/1/4.
+//  Copyright © 2019 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
