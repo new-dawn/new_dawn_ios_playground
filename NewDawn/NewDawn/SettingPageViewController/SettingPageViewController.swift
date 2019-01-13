b1280c7f35793bb39cbc78257e7c9f538c94d89f
Charlespartina <tangziyi001@gmail.com>
2019-01-13 12:08:58 -0500

    Update

---
+++ NewDawn/NewDawn/SettingPageViewController/SettingPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingPageViewController.swift
+//  NewDawn
+//
+//  Created by 汤子毅 on 2019/1/13.
+//  Copyright © 2019 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class SettingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
