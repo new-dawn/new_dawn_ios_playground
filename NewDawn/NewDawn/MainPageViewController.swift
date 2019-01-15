0b0d8831919b05a71f606fc8d828d23c8237a94c
Charlespartina <tangziyi001@gmail.com>
2019-01-14 22:09:07 -0500

    Multiple Profiles

--- NewDawn/NewDawn/MainPageViewController.swift
+++ NewDawn/NewDawn/MainPageViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
+let MAIN_PAGE_PROFILE_INDEX = "main_page_profile_index"
+
 class MainPageViewController: UIViewController {
     
-    fileprivate let viewModel = MainPageViewModel(userProfile: UserProfile(data: USER_DUMMY_DATA))
     @IBOutlet weak var tableView: UITableView!
+    var viewModel: MainPageViewModel!
     
+    func loadProfileIndex() -> Int {
+        if let index = self.localReadKeyValue(key: MAIN_PAGE_PROFILE_INDEX) as? Int {
+            return index
+        }
+        self.localStoreKeyValue(key: MAIN_PAGE_PROFILE_INDEX, value: 0)
+        return 0
+    }
+
+    func updateProfileIndex() -> Void {
+        self.localStoreKeyValue(key: MAIN_PAGE_PROFILE_INDEX, value: loadProfileIndex() + 1)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        if loadProfileIndex() >= USER_DUMMY_DATA.count {
+            // TODO: When the user has seen all profiles, we go back to the first profile.
+            // In the future, we should go to an ending page
+            self.localStoreKeyValue(key: MAIN_PAGE_PROFILE_INDEX, value: 0)
+        }
+        viewModel = MainPageViewModel(userProfile: UserProfile(data: USER_DUMMY_DATA[loadProfileIndex()]))
         tableView.dataSource = viewModel
         tableView.delegate = viewModel
         tableView.rowHeight = UITableView.automaticDimension
@@ -22,4 +42,7 @@ class MainPageViewController: UIViewController {
         navigationItem.hidesBackButton = true
     }
 
+    @IBAction func skipButtonTapped(_ sender: Any) {
+        updateProfileIndex()
+    }
 }
