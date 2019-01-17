2389910dc74a388a567a87ccc1157ace87e27217
l951010 <ljliniphone@gmail.com>
2019-01-17 00:50:07 -0500

    all profile response from backend

--- NewDawn/NewDawn/MainPageViewController.swift
+++ NewDawn/NewDawn/MainPageViewController.swift
@@ -29,6 +29,7 @@ class MainPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getUserData()
         if loadProfileIndex() >= USER_DUMMY_DATA.count {
             // TODO: When the user has seen all profiles, we go back to the first profile.
             // In the future, we should go to an ending page
@@ -45,4 +46,82 @@ class MainPageViewController: UIViewController {
     @IBAction func skipButtonTapped(_ sender: Any) {
         updateProfileIndex()
     }
+    
+    func getUserData(){
+        let request = createGetProfileRequest()
+        processSessionTasks(request: request!, callback: readReponse)
+    }
+    
+    func parseUserData(json: NSDictionary){
+
+    }
+    
+    func readReponse(parseJSON: NSDictionary) -> Void{
+        print(parseJSON)
+        let msg = parseJSON["success"] as? Bool
+        if msg == false {
+            print("Registration Data Failed to Send")
+            return
+        }
+    }
+    
+    func encodeParams(raw_params: Dictionary<String, String>) -> String{
+        var params:String = ""
+        var n = 0
+        for (key, value) in raw_params{
+            if n == 0{
+                params = "?" + key + "=" + value
+            }else{
+                params = params + "&" + key + "=" + value
+            }
+            n += 1
+        }
+        return params
+    }
+    
+    func createGetProfileRequest() -> URLRequest? {
+        // TODO: get username and api_key from keychain/local storage
+        let psudo_params = [
+            "username":"duckmoll",
+            "api_key":"f1c085c2ed2196cb029827c80f879a2ef3ce2189"
+        ]
+        let params = encodeParams(raw_params: psudo_params)
+        let url = getURL(path: "profile/" + params)
+        var request = URLRequest(url:url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("ApiKey", forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    struct User:Decodable{
+        let date_joined: String
+        let email: String
+        let first_name: String
+        let is_active: Int
+        let id: Int
+        let is_superuser: Int
+        let last_login: String
+        let last_name: String
+        let resource_uri: String
+        let username: String
+    }
+    
+    struct UserProfileStruct: Decodable {
+        let birthday: String
+        let city_preference: String
+        let creation_date: String
+        let gender: String
+        let id: Int
+        let name: String
+        let phone_number: String
+        let resource_uri: String
+        let user: [User]
+    }
+    
+    struct backendGetResponse: Decodable {
+        let meta: String
+        let objectts: [UserProfileStruct]
+    }
+    
 }
