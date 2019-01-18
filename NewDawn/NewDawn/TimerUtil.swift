2055c0360d52239baa57a764d2c814f6cb78ea5e
Charlespartina <tangziyi001@gmail.com>
2019-01-17 21:19:43 -0500

    Make Always update to be true

--- NewDawn/NewDawn/TimerUtil.swift
+++ NewDawn/NewDawn/TimerUtil.swift
@@ -68,7 +68,7 @@ class TimerUtil {
     }
     
     // A helper function to check if current date surpasses the latest date
-    static func isOutdated(always: Bool = false) -> Bool {
+    static func isOutdated(always: Bool = true) -> Bool {
         // TODO: Make always default to false
         return always || getCurrentDate() > readStoredDate()
     }
