01207ac1d749a97ab513bd58913637155baadbd3
l951010 <ljliniphone@gmail.com>
2019-01-21 14:37:28 -0500

    unforce parse

--- NewDawn/NewDawn/Models/UserModels.swift
+++ NewDawn/NewDawn/Models/UserModels.swift
@@ -209,12 +209,12 @@ class UserProfileBuilder{
     }
     
     static func parseProfileInfo(profile_data: [String: Any])-> [String: Any]{
-        let user_data = profile_data["user"]! as? [String: Any]
+        let user_data = profile_data["user"] as? [String: Any]
         var info = [
-            FIRSTNAME: user_data![FIRSTNAME]! as! String,
-            LASTNAME: user_data![LASTNAME]! as! String,
-            DEGREE: profile_data[DEGREE]! as! String,
-            SCHOOL: profile_data[SCHOOL]! as! String,
+            FIRSTNAME: user_data?[FIRSTNAME] as? String ?? UNKNOWN,
+            LASTNAME: user_data?[LASTNAME] as? String ?? UNKNOWN,
+            DEGREE: profile_data[DEGREE] as? String ?? UNKNOWN,
+            SCHOOL: profile_data[SCHOOL] as? String ?? UNKNOWN,
             "images": [
                 [
                     "media": "media/images/testcat.JPG",
@@ -223,15 +223,17 @@ class UserProfileBuilder{
             ],
             "question_answers":[String]()
             ] as [String : Any]
-        for answer_question in (profile_data["answer_question"] as? [[String: Any]])!{
-            let answer_question_dict = [
-                "id": answer_question["order"]!,
-                QUESTION: answer_question[QUESTION]!,
-                ANSWER: answer_question[ANSWER]!
-                ] as [String : Any]
-            var q_a_temp = info["question_answers"] as? [[String: Any]] ?? [[String: Any]]()
-            q_a_temp.append(answer_question_dict)
-            info["question_answers"] = q_a_temp
+        if let answer_questions = profile_data["answer_question"] as? [[String: Any]]{
+            for answer_question in answer_questions{
+                let answer_question_dict = [
+                    "id": answer_question["order"] ?? UNKNOWN,
+                    QUESTION: answer_question[QUESTION] ?? UNKNOWN,
+                    ANSWER: answer_question[ANSWER] ?? UNKNOWN
+                    ] as [String : Any]
+                var q_a_temp = info["question_answers"] as? [[String: Any]] ?? [[String: Any]]()
+                q_a_temp.append(answer_question_dict)
+                info["question_answers"] = q_a_temp
+            }
         }
         return info
     }
