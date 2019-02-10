750450f9a95a27e799db76f68380b5409ef39e4d
l951010 <ljliniphone@gmail.com>
2019-02-10 16:28:09 -0500

    setting Init

---
+++ NewDawn/NewDawn/ProfileSettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileSettingViewController.swift
+//  NewDawn
+//
+//  Created by Junlin Liu on 2/10/19.
+//  Copyright © 2019 New Dawn. All rights reserved.
+//
+
+import UIKit
+
+class ProfileSettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+}
