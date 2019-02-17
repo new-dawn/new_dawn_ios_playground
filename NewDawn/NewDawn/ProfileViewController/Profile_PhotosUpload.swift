d7654a9626df27f1add95b72c21278d0e02fce13
l951010 <ljliniphone@gmail.com>
2019-02-17 01:09:46 -0500

    Collection view with image picker init

--- NewDawn/NewDawn/ProfileViewController/Profile_PhotosUpload.swift
+++ NewDawn/NewDawn/ProfileViewController/Profile_PhotosUpload.swift
@@ -8,59 +8,92 @@
 
 import UIKit
 
-class Profile_PhotosUpload: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class Profile_PhotosUpload: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
 
-    @IBOutlet weak var imageView: UIImageView!
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionViewCell
+        cell.backgroundColor = UIColor.gray
+        cell.myImage.image = imagesArray[indexPath.row] as? UIImage
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        clicked_image = indexPath.row
+        let alert = UIAlertController(title: "Photo", message: "Choose Photo", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in}))
+        
+        alert.addAction(UIAlertAction(title: "Gallary", style: .default, handler: { (action) in
+            
+            self.gallary()
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in}))
+        
+        present(alert,animated: true,completion: nil)
+    }
+    
+    
+    func gallary() {
+        
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker,animated: true ,completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let editedView = info[.editedImage] as! UIImage
+        imagesArray.removeObject(at: clicked_image)
+        imagesArray.insert(editedView, at: clicked_image)
+//        imagesArray.add(editedView)
+        dismiss(animated: true, completion: nil)
+        
+        
+        collectionView.reloadData()
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let picker = UIImagePickerController()
+    var clicked_image = 0
+    var button = UIImage(named: "LikeButton")
+    var imagesArray = NSMutableArray()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagesArray = [button!, button!, button!, button!, button!, button!]
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        picker.delegate = self
+        
+        let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
+        layout.minimumInteritemSpacing = 5
+        layout.itemSize = CGSize(width: (self.collectionView.frame.size.width - 20) / 3, height: 100)
+        
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let image = info[.editedImage] as? UIImage{
-            imageView.image = image
-        }else if let image = info[.originalImage] as? UIImage {
-            imageView.image = image
-        } else {
-            
-            print("There was a problem getting the image")
-            
-        }
-        
-        self.dismiss(animated: true, completion: nil)
-        
-    }
-
-
-    @IBAction func chooseImage(_ sender: Any) {
-        
-        let imagePickerController = UIImagePickerController()
-        
-        imagePickerController.delegate = self
-//        let actionSheet = UIAlertController()
-        imagePickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
-        
-        imagePickerController.allowsEditing = true
-        
-        
-        self.present(imagePickerController, animated: true, completion: nil)
-    }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
