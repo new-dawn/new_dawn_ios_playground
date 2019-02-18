5ac5d2b131bf216226dd763c707b087ebd79bce8
Charlespartina <tangziyi001@gmail.com>
2019-02-18 11:51:34 -0800

    integrate update

--- NewDawn/NewDawn/ChatPageViewController/ChatRoomViewController.swift
+++ NewDawn/NewDawn/ChatPageViewController/ChatRoomViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 import MessageKit
 import MessageInputBar
+import PusherSwift
 
 class ChatRoomViewController: MessagesViewController {
+    
+    let PUSHER_APP_KEY = "6cc619d64bfad1da062a"
+    let TEST_CHANNEL = "test_channel"
+    let CHAT_EVENT = "chat_event"
+    let MESSAGE = "message"
+    let CLUSTER = "us2"
+
     // Display as view title
     // To be changed by Chat page segue
     // when a cell is clicked
@@ -19,6 +27,29 @@ class ChatRoomViewController: MessagesViewController {
     var senderA: Sender?
     var senderB: Sender?
     var messages: [MessageType] = []
+    var pusher: Pusher!
+    
+    func subscribeToChat() -> Void {
+        let options = PusherClientOptions(
+            host: .cluster(CLUSTER)
+        )
+        pusher = Pusher(
+            key: PUSHER_APP_KEY,
+            options: options
+        )
+        // subscribe to channel and bind to event
+        let channel = pusher.subscribe(TEST_CHANNEL)
+        let _ = channel.bind(eventName: CHAT_EVENT, callback: { (data: Any?) -> Void in
+            if let data = data as? [String : AnyObject] {
+                if let message = data[self.MESSAGE] as? String, let user_id = data[ID] as? String {
+                    print(message)
+                    let sender = Sender(id: user_id, displayName: user_id)
+                    self.insertMessage(Message(sender: sender, content: message))
+                }
+            }
+        })
+        pusher.connect()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,9 +66,41 @@ class ChatRoomViewController: MessagesViewController {
             Message(sender: senderB!, content: "Of course! When are you available this week?")
         ]
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        subscribeToChat()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        pusher.disconnect()
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.title = userNameYou
+    }
+    
+    func isLastSectionVisible() -> Bool {
+        guard !messages.isEmpty else { return false }
+        let lastIndexPath = IndexPath(item: 0, section: messages.count - 1)
+        return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
+    }
+    
+    func insertMessage(_ message: MessageType) {
+        messages.append(message)
+        // Reload last section to update header/footer labels and insert a new one
+        messagesCollectionView.performBatchUpdates({
+            messagesCollectionView.insertSections([messages.count - 1])
+            if messages.count >= 2 {
+                messagesCollectionView.reloadSections([messages.count - 2])
+            }
+        }, completion: { [weak self] _ in
+            if self?.isLastSectionVisible() == true {
+                self?.messagesCollectionView.scrollToBottom(animated: true)
+            }
+        })
     }
 }
 
@@ -59,9 +122,9 @@ extension ChatRoomViewController: MessagesDisplayDelegate, MessagesLayoutDelegat
 extension ChatRoomViewController: MessageInputBarDelegate {
     
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
-//        
+//
 //        for component in inputBar.inputTextView.components {
-//            
+//
 //            if let str = component as? String {
 //                let message = MockMessage(text: str, sender: currentSender(), messageId: UUID().uuidString, date: Date())
 //                insertMessage(message)
@@ -69,7 +132,7 @@ extension ChatRoomViewController: MessageInputBarDelegate {
 //                let message = MockMessage(image: img, sender: currentSender(), messageId: UUID().uuidString, date: Date())
 //                insertMessage(message)
 //            }
-//            
+//
 //        }
 //        inputBar.inputTextView.text = String()
 //        messagesCollectionView.scrollToBottom(animated: true)
