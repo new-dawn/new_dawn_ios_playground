7a419d482d5f19f50de5735948f41f4959a4d0a2
Charlespartina <tangziyi001@gmail.com>
2019-05-13 21:54:56 -0400

    polish ui

--- NewDawn/NewDawn/ViewControllers/MainPageViewControllers/RectInfoViewController.swift
+++ NewDawn/NewDawn/ViewControllers/MainPageViewControllers/RectInfoViewController.swift
@@ -12,20 +12,36 @@ class RectInfoViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var yourImageView: UIImageView!
     @IBOutlet weak var yourCommentTextView: UITextView!
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var myAnswerTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Get my profile photo
+        // Get my profile photo and firstname
         LoginUserUtil.fetchLoginUserProfile() {
             user_profile in
-            if user_profile?.mainImages.first != nil {
-                self.myImageView.downloaded(from: self.myImageView.getURL(path: user_profile!.mainImages.first!.image_url))
+            if let my_image_url = user_profile?.mainImages.first?.image_url {
+                self.myImageView.downloaded(
+                    from: self.myImageView.getURL(path: my_image_url))
+                ImageUtil.polishCircularImageView(imageView: self.myImageView)
+            }
+            if let my_first_name = user_profile?.firstname {
+                let like_me_info = LikeInfoUtil.getLatestLikeMeInfo()
+                DispatchQueue.main.async {
+                    if let my_question = like_me_info?.likedInfo.liked_question {
+                        self.questionTextView.text = my_question
+                    }
+                    if let my_answer = like_me_info?.likedInfo.liked_answer {
+                        self.myAnswerTextView.text = "\(my_first_name): \(my_answer)"
+                    }
+                }
             }
         }
         // Get your image and comment
         let like_me_info = LikeInfoUtil.getLatestLikeMeInfo()
         if let your_image_url = like_me_info?.yourImageURL {
              yourImageView.downloaded(from: yourImageView.getURL(path: your_image_url))
+             ImageUtil.polishCircularImageView(imageView: self.yourImageView)
         }
         if let your_comment = like_me_info?.likedInfo.liked_message, let your_first_name = like_me_info?.yourFirstName {
             yourCommentTextView.text = "\(your_first_name): \(your_comment)"
@@ -36,28 +52,4 @@ class RectInfoViewController: UIViewController {
         dismiss(animated: true)
     }
     
-}
-
-class RectInfoQuestionAnswerViewController: RectInfoViewController {
-    
-    @IBOutlet weak var questionTextView: UITextView!
-    @IBOutlet weak var myAnswerTextView: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Get my first name
-        LoginUserUtil.fetchLoginUserProfile() {
-            user_profile in
-            if let my_first_name = user_profile?.firstname {
-                let like_me_info = LikeInfoUtil.getLatestLikeMeInfo()
-                if let my_question = like_me_info?.likedInfo.liked_question {
-                    self.questionTextView.text = my_question
-                }
-                if let my_answer = like_me_info?.likedInfo.liked_answer {
-                    self.myAnswerTextView.text = "\(my_first_name): \(my_answer)"
-                }
-            }
-        }
-    }
 }
