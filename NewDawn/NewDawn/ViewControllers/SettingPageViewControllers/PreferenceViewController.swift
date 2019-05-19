89f950025242b5e8848bd5b0e39894966e8fcc9c
l951010 <ljliniphone@gmail.com>
2019-05-19 17:47:25 -0400

    new Pref UI

--- NewDawn/NewDawn/ViewControllers/SettingPageViewControllers/PreferenceViewController.swift
+++ NewDawn/NewDawn/ViewControllers/SettingPageViewControllers/PreferenceViewController.swift
@@ -9,64 +9,57 @@
 import Foundation
 import UIKit
 
-let GENDER_PREF = "Gender_PREF"
-let DISTANCE_PREF = "Distance_PREF"
-let AGE_PREF = "Age_PREF"
-let HEIGHT_PREF = "Height_PREF"
-let EDUCATION_PREF = "Education_PREF"
 
 class PreferenceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var clickable_fitlers = ["Gender", "Distance", "Age Range", "Height", "Education"]
-
+    var clickable_filters = ["age_pref", "height_pref"]
+    var clickable_filters_name = ["年龄范围", "身高范围"]
+    var icons = ["HeightIcon", "HeightIcon"]
     @IBOutlet weak var prefTableView: UITableView!
     
     override func viewDidLoad() {
-        self.prefTableView.register(UITableViewCell.self, forCellReuseIdentifier: "pref_cell")
         prefTableView.dataSource = self
         prefTableView.delegate = self
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = self.prefTableView.dequeueReusableCell(withIdentifier: "pref_cell")!
-        cell.textLabel?.text = clickable_fitlers[indexPath.section]
+        let cell:PrefSettingCell = self.prefTableView.dequeueReusableCell(withIdentifier: "pref_cell") as! PrefSettingCell
         
-        // UI for table view cells
-        cell.textLabel?.font = UIFont(name:"PingFangTC-Semibold", size: 18.0)
+        
+        // UI for Table View Layer
         cell.backgroundColor = UIColor.white
         cell.layer.borderColor = UIColor(red:151/255, green:151/255, blue:151/255, alpha:1).cgColor
         cell.layer.borderWidth = 1
         cell.layer.cornerRadius = 20
         cell.clipsToBounds = true
         
-        // UIButton for subsidiary view
-        let pref_button = UIButton(frame: CGRect(x: 1, y: 1, width: 48, height: 20))
-        pref_button.tintColor = UIColor(red:151/255, green:151/255, blue:151/255, alpha:1)
-        
-        // Get pref from local storage or default text
-        let accessory_text = LocalStorageUtil.localReadKeyValue(key: clickable_fitlers[indexPath.section] + "_PREF") ?? "Add Preference"
-        pref_button.setTitle((accessory_text as! String),for: .normal)
-        pref_button.setTitleColor(UIColor.gray, for: .normal)
-        let rect = CGRect(x: 20, y: 100, width: 150, height: 100)
-        pref_button.tag = indexPath.section
-        pref_button.addTarget(self, action: #selector(sonView(_:)), for: .touchDown)
-        cell.accessoryView = pref_button
-        cell.accessoryView?.frame = rect
+        // UI for Table view Cell
+        cell.prefname.text = self.clickable_filters_name[indexPath.section]
+        cell.prefvalue.text = getPreferenceValue(pref: self.clickable_filters[indexPath.section])
+        cell.icon.image = UIImage(named: self.icons[indexPath.section])
+        cell.button.tag = indexPath.section
+        cell.button.addTarget(self, action: #selector(sonView(_:)), for: .touchDown)
+   
         
         return cell
     }
     
+    func getPreferenceValue(pref: String) -> String{
+        let stored_pref = LocalStorageUtil.localReadKeyValue(key: pref) ?? ""
+        return stored_pref as! String
+    }
+    
     @objc func sonView(_ sender: UIButton){
-        let son_view = clickable_fitlers[sender.tag] + "_PREF"
+        let son_view = clickable_filters[sender.tag]
         self.performSegue(withIdentifier: son_view, sender: self)
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return self.clickable_fitlers.count
+        return self.clickable_filters.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
---
+++ NewDawn/PrefSettingTableCell.swift
@@ -0,0 +1,19 @@
+//
+//  PrefTableCell.swift
+//  NewDawn
+//
+//  Created by Junlin Liu on 5/19/19.
+//  Copyright © 2019 New Dawn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PrefSettingCell: UITableViewCell{
+    
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var prefname: UILabel!
+    @IBOutlet weak var prefvalue: UILabel!
+    @IBOutlet weak var button: UIButton!
+}
