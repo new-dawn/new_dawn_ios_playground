603af7e15117cf2bddd162060cbc3d7ce45d6485
Janice Zhou <jz3031@nyu.edu>
2019-05-27 02:07:01 -0400

    phone verification page new pop up

--- NewDawn/NewDawn/ViewControllers/LoginViewControllers/PhoneAuthenticateViewController.swift
+++ NewDawn/NewDawn/ViewControllers/LoginViewControllers/PhoneAuthenticateViewController.swift
@@ -65,10 +65,29 @@ class PhoneAuthenticateViewController: UIViewController {
     @IBAction func reVerifyButtonTapped(_ sender: Any) {
         // Popup menu for re-verification
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Retype Phone Number", style: .default) { _ in
+        alert.addAction(UIAlertAction(title: "其他手机号码", style: .default) { _ in
             self.performSegue(withIdentifier: "reVerify", sender: self.userPhoneNumber)
         })
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "再寄出一组验证码", style: .default) { _ in
+            // Create Activity Indicator
+            let activityIndicator = self.prepareActivityIndicator()
+            
+            // Send Request
+            let request = self.createPhoneVerifyRequest()
+            if request == nil {
+                // Request Creation Failed
+                return
+            }
+            
+            // Process Request
+            self.processSessionTasks(request: request!) {
+                response in
+                // Remove activity indicator
+                self.removeActivityIndicator(activityIndicator: activityIndicator)
+                self.readPhoneVerifyResponse(parseJSON: response)
+            }
+        })
+        alert.addAction(UIAlertAction(title: "取消", style: UIAlertAction.Style.cancel, handler: nil))
         present(alert, animated: true)
     }
 
@@ -132,6 +151,37 @@ class PhoneAuthenticateViewController: UIViewController {
             appDelegate?.window??.rootViewController = profileGNBPage
         }
     }
+    
+    // Create Phoen Verify request according to API spec
+    func createPhoneVerifyRequest() -> URLRequest? {
+        let url = HttpUtil.getURL(path: "user/phone_verify/request/")
+        var request = URLRequest(url:url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let postString = [
+            "phone_number": userPhoneNumber,
+            "country_code": "1"
+            ] as [String: String]
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: postString, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+            self.displayMessage(userMessage: "Phone Verify Request Creation Failed", dismiss: false)
+            return nil
+        }
+        return request
+    }
+    
+    // A Callback function to handle phone verify response
+    func readPhoneVerifyResponse(parseJSON: NSDictionary) -> Void {
+        let msg = parseJSON["success"] as? Bool
+        if msg == false {
+            self.displayMessage(userMessage: "Phone Number Verification Failed to Send")
+            return
+        }
+    }
+    
     
     /*
     // MARK: - Navigation
