04e823e71039f9bdecf5214aff59c687d7ed1410
l951010 <ljliniphone@gmail.com>
2019-06-20 22:03:47 -0400

    education

--- NewDawn/NewDawn/ViewControllers/SettingPageViewControllers/EditProfileSubViewController.swift
+++ NewDawn/NewDawn/ViewControllers/SettingPageViewControllers/EditProfileSubViewController.swift
@@ -380,3 +380,104 @@ class EditProfileBasicInfoViewController: UIViewController, UIPickerViewDelegate
         return heightStringArray
     }
 }
+
+class EditProfile_Education: UIViewController {
+    
+    var degree = UNKNOWN
+    // Constant string keys
+    let VISIBLE = "edu_visible"
+    
+    var visibleField = false
+    
+    
+    @IBOutlet weak var undergrad: UIButton!
+    @IBOutlet weak var grad: UIButton!
+    @IBOutlet weak var phd: UIButton!
+    
+    
+    @IBOutlet weak var schoolTextField: UITextField!
+    // Load fields that user has already filled in
+    func loadStoredFields() {
+        if let school = localReadKeyValue(key: SCHOOL) as? String {
+            schoolTextField.text = school
+        }
+        if let stored_degree = localReadKeyValue(key: DEGREE) as? String {
+            degree = stored_degree
+            if degree == "本科" {
+                selectDegreeButton(button: undergrad)
+            }
+            else if degree == "硕士" {
+                selectDegreeButton(button: grad)
+            }
+            else {
+                selectDegreeButton(button: phd)
+            }
+        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        schoolTextField.setBottomBorder()
+        polishDegreeButton(button: undergrad)
+        polishDegreeButton(button: grad)
+        polishDegreeButton(button: phd)
+        loadStoredFields()
+        overrideBackbutton()
+    }
+    
+    func overrideBackbutton(){
+        self.navigationItem.hidesBackButton = true
+        let newBackButton = UIBarButtonItem(title: "< Profile Edit", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.back(sender:)))
+        self.navigationItem.leftBarButtonItem = newBackButton
+    }
+    
+    @objc func back(sender: UIBarButtonItem){
+        localStoreKeyValue(key: SCHOOL, value: schoolTextField.text!)
+        self.dismiss(animated: true, completion: {})
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func undergradTapped(_ sender: Any) {
+        selectDegreeButton(button: undergrad)
+        deselectButton(button: grad)
+        deselectButton(button: phd)
+        degree = "本科"
+        localStoreKeyValue(key: DEGREE, value: degree)
+    }
+    
+    @IBAction func gradTapped(_ sender: Any) {
+        selectDegreeButton(button: grad)
+        deselectButton(button: undergrad)
+        deselectButton(button: phd)
+        degree = "硕士"
+        localStoreKeyValue(key: DEGREE, value: degree)
+    }
+    
+    @IBAction func phdTapped(_ sender: Any) {
+        selectDegreeButton(button: phd)
+        deselectButton(button: undergrad)
+        deselectButton(button: grad)
+        degree = "博士"
+        localStoreKeyValue(key: DEGREE, value: degree)
+    }
+    
+    func polishDegreeButton(button: UIButton) -> Void {
+        button.layer.cornerRadius = 13
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = UIColor(red:151/255, green:151/255, blue:151/255, alpha:1).cgColor
+        button.layer.masksToBounds = true
+    }
+    
+    func selectDegreeButton(button: UIButton) -> Void {
+        button.layer.cornerRadius = 13
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = UIColor(red:22/255, green:170/255, blue:184/255, alpha:1).cgColor
+        button.layer.backgroundColor = UIColor(red:22/255, green:170/255, blue:184/255, alpha:1).cgColor
+        button.layer.masksToBounds = true
+    }
+}
+
+
+
