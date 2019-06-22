6b12aa7e00fa73fbbc890ed6bef7465efe47aeda
Charlespartina <tangziyi001@gmail.com>
2019-06-22 10:39:04 -0400

    location modify

--- NewDawn/NewDawn/ViewControllers/RegistrationViewControllers/Profile_HomeTown.swift
+++ NewDawn/NewDawn/ViewControllers/RegistrationViewControllers/Profile_HomeTown.swift
@@ -5,32 +5,19 @@
 //  Created by macboy on 12/23/18.
 //  Copyright © 2018 New Dawn. All rights reserved.
 //
-
 import UIKit
 
 let HOMETOWN = "hometown"
 
-class Profile_HomeTown: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class Profile_HomeTown: UIViewController {
     
+    
+    /* Constant String Keys*/
     let VISIBLE = "hometown_visible"
-    let locationPickerData = ["纽约", "波士顿"]
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
+    /* Constrains */
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return locationPickerData.count
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return locationPickerData[row]
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        hometownTextField.text = locationPickerData[row]
-    }
-    
+    //    var visibleField = false
     
     @IBOutlet weak var hometownTextField: UITextField!
     @IBOutlet weak var continueButton: UIButton!
@@ -42,10 +29,6 @@ class Profile_HomeTown: UIViewController, UIPickerViewDelegate, UIPickerViewData
         }
     }
     
-    @objc func donePicker() {
-        hometownTextField.resignFirstResponder()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         hometownTextField.setBottomBorder()
@@ -53,31 +36,17 @@ class Profile_HomeTown: UIViewController, UIPickerViewDelegate, UIPickerViewData
         backButton.titleEdgeInsets = UIEdgeInsets(top: -20.0, left: 0.0, bottom: 0.0, right: 0.0)
         loadStoredFields()
         
-        // Picker Toolbar
-        let toolbar = UIToolbar();
-        toolbar.sizeToFit()
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: #selector(Profile_HomeTown.donePicker))
-        toolbar.setItems([flexSpace, doneButton], animated: false)
-        toolbar.isUserInteractionEnabled = true
-        
-        // Location Picker
-        let locationPicker = UIPickerView()
-        hometownTextField.inputView = locationPicker
-        hometownTextField.inputAccessoryView = toolbar
-        locationPicker.delegate = self
-        
     }
-
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool{
-        if (hometownTextField.text?.isEmpty)! {
+        if (hometownTextField.text?.isEmpty)! && identifier == "hometown_continue" {
             self.displayMessage(userMessage: "Cannot have empty field")
             return false
         }else{
             return true
         }
     }
-
+    
     @IBAction func nextButtonTapped(_ sender: Any) {
         localStoreKeyValue(key: HOMETOWN, value: hometownTextField.text!)
         localStoreKeyValue(key: VISIBLE, value: true)
