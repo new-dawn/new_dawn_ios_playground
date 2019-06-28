f1118badd4e121bb23c6493a4e8d575e0e1b3875
Janice Zhou <jz3031@nyu.edu>
2019-06-28 13:56:42 +0800

    immediately untaken fixed

--- NewDawn/NewDawn/ViewControllers/ChatPageViewControllers/ChatRoomViewController.swift
+++ NewDawn/NewDawn/ViewControllers/ChatPageViewControllers/ChatRoomViewController.swift
@@ -327,54 +327,61 @@ class ChatRoomViewController: MessagesViewController {
     }
     
     @IBAction func imTakenButtonTapped(_ sender: Any) {
-        fetchEndUserProfile() {
-            profile in
-            if profile.takenBy == -1 {
-                let alertController = UIAlertController(title: "专属模式", message: "确认向" + self.userNameYou + "发出“专属”邀请吗？" + self.userNameYou + "在接受邀请后，你们的资料就不再对第三方可见，无法和第三方聊天，也无法再进行新的匹配。专属模式可以随时取消，对方会收到提醒。详情请见帮助菜单。", preferredStyle: .alert)
-                let paragraphStyle = NSMutableParagraphStyle()
-                paragraphStyle.alignment = NSTextAlignment.left
-                let messageText = NSMutableAttributedString(
-                    string: "确认向" + self.userNameYou + "发出“专属”邀请吗？" + self.userNameYou + "在接受邀请后，你们的资料就不再对第三方可见，无法和第三方聊天，也无法再进行新的匹配。专属模式可以随时取消，对方会收到提醒。详情请见帮助菜单。",
-                    attributes: [
-                        NSAttributedString.Key.paragraphStyle: paragraphStyle,
-                        NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13.0)
-                    ]
-                )
-                alertController.setValue(messageText, forKey: "attributedMessage")
-                self.present(alertController, animated: true, completion: nil)
-                let confirmAction = UIAlertAction(title: "确定", style: .default) {(_) in
-                    HttpUtil.sendAction(user_from: self.userIdMe, user_to: self.userIdYou, action_type: UserActionType.REQUEST_TAKEN.rawValue, entity_type: EntityType.NONE.rawValue, entity_id: 0, message: UNKNOWN)
-                    let sentAlertController = UIAlertController(title: nil, message: "专属邀请已成功寄出。", preferredStyle: .alert)
-                    self.present(sentAlertController, animated: true, completion: nil)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        sentAlertController.dismiss(animated: true, completion: nil)
-                    }
+        LoginUserUtil.fetchLoginUserProfile(readLocal: false) {
+            profile, error in
+            if error != nil {
+                DispatchQueue.main.async {
+                    self.displayMessage(userMessage: "Error: Fetch Login User Profile Failed: \(error!)")
                 }
-                let cancelAction = UIAlertAction(title: "取消", style: .default)
-                alertController.addAction(cancelAction)
-                alertController.addAction(confirmAction)
+                return
             }
-            else {
-                let alertController = UIAlertController(title: "专属模式", message: "确认与" + self.userNameYou + "解除“专属”模式吗？解除后，对方会收到提醒。你们的资料将重新对第三方可见。详情请见帮助菜单。", preferredStyle: .alert)
-                self.present(alertController, animated: true, completion: nil)
-                let confirmAction = UIAlertAction(title: "确定", style: .default) {(_) in
-                    HttpUtil.sendAction(user_from: self.userIdMe, user_to: self.userIdYou, action_type: UserActionType.UNTAKEN.rawValue, entity_type: EntityType.NONE.rawValue, entity_id: 0, message: UNKNOWN)
-                    let removeAlertController = UIAlertController(title: nil, message: "专属模式已成功解除。", preferredStyle: .alert)
-                    self.present(removeAlertController, animated: true, completion: nil)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        removeAlertController.dismiss(animated: true, completion: nil)
+            DispatchQueue.main.async {
+                if profile?.takenBy == -1 {
+                    let alertController = UIAlertController(title: "专属模式", message: "确认向" + self.userNameYou + "发出“专属”邀请吗？" + self.userNameYou + "在接受邀请后，你们的资料就不再对第三方可见，无法和第三方聊天，也无法再进行新的匹配。专属模式可以随时取消，对方会收到提醒。详情请见帮助菜单。", preferredStyle: .alert)
+                    let paragraphStyle = NSMutableParagraphStyle()
+                    paragraphStyle.alignment = NSTextAlignment.left
+                    let messageText = NSMutableAttributedString(
+                        string: "确认向" + self.userNameYou + "发出“专属”邀请吗？" + self.userNameYou + "在接受邀请后，你们的资料就不再对第三方可见，无法和第三方聊天，也无法再进行新的匹配。专属模式可以随时取消，对方会收到提醒。详情请见帮助菜单。",
+                        attributes: [
+                            NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13.0)
+                        ]
+                    )
+                    alertController.setValue(messageText, forKey: "attributedMessage")
+                    self.present(alertController, animated: true, completion: nil)
+                    let confirmAction = UIAlertAction(title: "确定", style: .default) {(_) in
+                        HttpUtil.sendAction(user_from: self.userIdMe, user_to: self.userIdYou, action_type: UserActionType.REQUEST_TAKEN.rawValue, entity_type: EntityType.NONE.rawValue, entity_id: 0, message: UNKNOWN)
+                        let sentAlertController = UIAlertController(title: nil, message: "专属邀请已成功寄出。", preferredStyle: .alert)
+                        self.present(sentAlertController, animated: true, completion: nil)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            sentAlertController.dismiss(animated: true, completion: nil)
+                        }
                     }
-                    let sendTakenimg = UIImage(named: "ImTakenButton")
-                    self.imTakenButton.setImage(sendTakenimg, for: .normal)
-                    self.messagesCollectionView.reloadData()
-                    self.messagesCollectionView.scrollToBottom()
+                    let cancelAction = UIAlertAction(title: "取消", style: .default)
+                    alertController.addAction(cancelAction)
+                    alertController.addAction(confirmAction)
                 }
-                let cancelAction = UIAlertAction(title: "取消", style: .default)
-                alertController.addAction(cancelAction)
-                alertController.addAction(confirmAction)
+                else {
+                    let alertController = UIAlertController(title: "专属模式", message: "确认与" + self.userNameYou + "解除“专属”模式吗？解除后，对方会收到提醒。你们的资料将重新对第三方可见。详情请见帮助菜单。", preferredStyle: .alert)
+                    self.present(alertController, animated: true, completion: nil)
+                    let confirmAction = UIAlertAction(title: "确定", style: .default) {(_) in
+                        HttpUtil.sendAction(user_from: self.userIdMe, user_to: self.userIdYou, action_type: UserActionType.UNTAKEN.rawValue, entity_type: EntityType.NONE.rawValue, entity_id: 0, message: UNKNOWN)
+                        let removeAlertController = UIAlertController(title: nil, message: "专属模式已成功解除。", preferredStyle: .alert)
+                        self.present(removeAlertController, animated: true, completion: nil)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            removeAlertController.dismiss(animated: true, completion: nil)
+                        }
+                        let sendTakenimg = UIImage(named: "ImTakenButton")
+                        self.imTakenButton.setImage(sendTakenimg, for: .normal)
+                        self.messagesCollectionView.reloadData()
+                        self.messagesCollectionView.scrollToBottom()
+                    }
+                    let cancelAction = UIAlertAction(title: "取消", style: .default)
+                    alertController.addAction(cancelAction)
+                    alertController.addAction(confirmAction)
+                }
             }
         }
-        
     }
     
     @IBAction func OptionsButtonTapped(_ sender: Any) {
